e6d8ba12d1d7c28003ed9738bb3a6a93ae096427
Vũ Thị Hường <huongvt201972gmail.com>
2018-09-25 22:48:06 +0700

    slide 2.2

---
+++ Bai_7_Monoalphabetic_TanSuatXuatHienChuTrongVB.cpp
@@ -0,0 +1,73 @@
+﻿
+/*
+* Bài tập 7:
++ Lập trình tần xuất xuất hiện các ký tự trong 1 văn bản xuất hiện bao nhiêu lần.
+*/
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+#include <string>
+
+#include<iostream>
+
+using namespace std;
+
+string M = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+int KiTu_So(char c) {
+
+	for (int i = 0; i < 26; i++) {
+
+		if (c == M[i]) {
+
+			return i;
+		}
+	}
+}
+
+char So_KiTu(int n) {
+
+	return M[n];
+}
+
+void SoLanXuatHien(string a) {
+
+	int b[27];
+
+	for (int i = 0; i < 27; i++) {
+
+		b[i] = 0;
+	}
+
+	for (int i = 0; i < a.size(); i++) {
+
+		for (int j = 0; j < M.size(); j++) {
+
+			if (a[i] == M[j]) {
+
+				b[j]++;
+			}
+			else {
+
+				b[26]++;
+			}
+		}
+	}
+
+	for (int i = 0; i < 27; i++) {
+
+		cout << M[i] << " : " << b[i] << endl;
+	}
+}
+
+int main() {
+
+	string s;
+
+	cout << "Nhap chuoi ki tu : ";
+	getline(cin, s);
+
+	SoLanXuatHien(s);
+		system("pause");
+		return 0;
+}
+
